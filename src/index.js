15f0d024b1db0aac8ba4513980b6e47e765c2d3b
backspacev9 <backspacev9@gmail.com>
2021-03-11 21:59:47 +0300

    feat: complete test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  var minimum = 0;
+  if (array !== undefined){
+    for (let i=0;i<array.length; i++){
+      if (array[i] < minimum) {
+        minimum = array[i];
+      }
+    }
+    return minimum;
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  var maximum = 0;
+  if (array !== undefined){
+    for (let i=0;i<array.length; i++){
+      if (array[i] > maximum) {
+        maximum = array[i];
+      }
+    }
+    return maximum;
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var avr = 0;
+  var sum = 0;
+  if (array !== undefined){
+    for (let i=0;i<array.length; i++){
+        sum = sum + array[i];
+        avr = sum/array.length;
+    }
+    return avr;
+  } else return 0;
 }
+
+
